415b6476726f3d4b3428d8a7dae02799daf1f611
adding table <zangjiahou@hotmail.com>
2018-10-14 19:11:13 -0400

    adding views.header

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="/">Learana</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item ">
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/discover-online-courses">Discover</a>
+                    </li>
+                    <li class="nav-item">
+
+                        <a class="nav-link" href="/why-learana">Why Learana?</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/meet-learana">Meet Learana</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/connect">Connect</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/get-started">Get Started</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                    <button class="btn btn btn-success my-2 my-sm-0" type="submit">Find Courses</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</header>
