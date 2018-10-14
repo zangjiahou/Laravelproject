9f01b11e293641ab5843deb629887061209a0aa5
adding table <zangjiahou@hotmail.com>
2018-10-14 19:31:05 -0400

    setting routes to pages.home

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    @include('includes.head')
+</head>>
+
+<header>
+    @include('includes.header')
+</header>>
+
+<main role="main">
+    @yield('content')
+<footer class="row">
+    @include('includes.footer')
+</footer>>
+</main>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.home')
+@section('content')
+    <h1> This is my homepage</h1>>
+@stop
